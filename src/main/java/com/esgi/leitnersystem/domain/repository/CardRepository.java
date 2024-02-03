fd395be963c45f19386e0e16ba302bdeb7ecae77
Restyled.io <commits@restyled.io>
2024-02-03 21:47:44 +0000

    Restyled by clang-format

--- src/main/java/com/esgi/leitnersystem/domain/repository/CardRepository.java
+++ src/main/java/com/esgi/leitnersystem/domain/repository/CardRepository.java
@@ -1,12 +1,9 @@
 package com.esgi.leitnersystem.domain.repository;
 
 import com.esgi.leitnersystem.domain.model.Card;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface CardRepository extends JpaRepository<Card, String> {
-
-}
+public interface CardRepository extends JpaRepository<Card, String> {}
--- src/main/java/com/esgi/leitnersystem/infrastructure/web/controller/CardsController.java
+++ src/main/java/com/esgi/leitnersystem/infrastructure/web/controller/CardsController.java
@@ -1,43 +1,46 @@
 package com.esgi.leitnersystem.infrastructure.web.controller;
 
-import com.esgi.leitnersystem.application.service.CardService;
-import org.springframework.web.bind.annotation.*;
-import com.esgi.leitnersystem.domain.model.Card;
 import com.esgi.leitnersystem.application.dto.CardUserData;
-import org.springframework.http.ResponseEntity;
-
+import com.esgi.leitnersystem.application.service.CardService;
+import com.esgi.leitnersystem.domain.model.Card;
 import java.util.List;
 import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cards")
 public class CardsController {
-    private final CardService cardService;
+  private final CardService cardService;
 
-    public CardsController(CardService cardService) {
-        this.cardService = cardService;
-    }
+  public CardsController(CardService cardService) {
+    this.cardService = cardService;
+  }
 
-    @GetMapping
-    public ResponseEntity<List<Card>> getAllCards() {
-        return ResponseEntity.ok().body(cardService.fetchAllCards());
-    }
+  @GetMapping
+  public ResponseEntity<List<Card>> getAllCards() {
+    return ResponseEntity.ok().body(cardService.fetchAllCards());
+  }
 
-    @PostMapping
-    public ResponseEntity<Card> createCard(@RequestBody CardUserData cardUserData) {
-        /* TODO: return a card */
-        return ResponseEntity.status(201).body(new Card());
-    }
+  @PostMapping
+  public ResponseEntity<Card>
+  createCard(@RequestBody CardUserData cardUserData) {
+    /* TODO: return a card */
+    return ResponseEntity.status(201).body(new Card());
+  }
 
-    @GetMapping("/quizz")
-    public ResponseEntity<List<Card>> getCardsForQuizz(@RequestParam(required = false) String date) {
-        // TODO: return cards for a quizz
-        return ResponseEntity.ok().body(List.of());
-    }
+  @GetMapping("/quizz")
+  public ResponseEntity<List<Card>>
+  getCardsForQuizz(@RequestParam(required = false) String date) {
+    // TODO: return cards for a quizz
+    return ResponseEntity.ok().body(List.of());
+  }
 
-    @PatchMapping("/{cardId}/answer")
-    public ResponseEntity<Void> answerCard(@PathVariable String cardId, @RequestBody Map<String, Boolean> answerBody) {
-        // TODO: handle the answer to a card
-        return ResponseEntity.noContent().build();
-    }
+  @PatchMapping("/{cardId}/answer")
+  public ResponseEntity<Void>
+  answerCard(@PathVariable String cardId,
+             @RequestBody Map<String, Boolean> answerBody) {
+    // TODO: handle the answer to a card
+    return ResponseEntity.noContent().build();
+  }
 }
