03aea04b1edd934a1840baec282dbc4467ddb8bc
Restyled.io <commits@restyled.io>
2024-02-17 19:58:39 +0000

    Restyled by clang-format

--- src/test/java/com/esgi/leitnersystem/cucumber/CategoryServiceSteps.java
+++ src/test/java/com/esgi/leitnersystem/cucumber/CategoryServiceSteps.java
@@ -1,5 +1,9 @@
 package com.esgi.leitnersystem.cucumber;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Mockito.*;
+
 import com.esgi.leitnersystem.domain.card.Card;
 import com.esgi.leitnersystem.domain.category.Category;
 import com.esgi.leitnersystem.domain.category.CategoryService;
@@ -8,49 +12,43 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static org.mockito.AdditionalAnswers.returnsFirstArg;
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 public class CategoryServiceSteps {
 
-    private Card card;
-    private CategoryService categoryService;
-    private CardRepository cardRepository;
+  private Card card;
+  private CategoryService categoryService;
+  private CardRepository cardRepository;
 
+  @Given("a card in category {string}")
+  public void a_card_in_category_first(String category) {
+    card = new Card();
+    card.setCategory(Category.valueOf(category));
+    cardRepository = mock(CardRepository.class);
+    when(cardRepository.save(any(Card.class))).then(returnsFirstArg());
+    categoryService = new CategoryService(cardRepository);
+  }
 
-    @Given("a card in category {string}")
-    public void a_card_in_category_first(String category) {
-        card = new Card();
-        card.setCategory(Category.valueOf(category));
-        cardRepository = mock(CardRepository.class);
-        when(cardRepository.save(any(Card.class))).then(returnsFirstArg());
-        categoryService = new CategoryService(cardRepository);
-    }
+  @When("the card is answered correctly")
+  public void the_card_is_answered_correctly() {
+    card = categoryService.promoteCard(card);
+  }
 
-    @When("the card is answered correctly")
-    public void the_card_is_answered_correctly() {
-        card = categoryService.promoteCard(card);
-    }
+  @Then("the card should be promoted to category {string}")
+  public void the_card_should_be_promoted_to_category(String expectedCategory) {
+    assertEquals(Category.valueOf(expectedCategory), card.getCategory());
+  }
 
-    @Then("the card should be promoted to category {string}")
-    public void the_card_should_be_promoted_to_category(String expectedCategory) {
-        assertEquals(Category.valueOf(expectedCategory), card.getCategory());
-    }
+  @When("the card is answered incorrectly")
+  public void the_card_is_answered_incorrectly() {
+    card = categoryService.demoteCardToFirst(card);
+  }
 
-    @When("the card is answered incorrectly")
-    public void the_card_is_answered_incorrectly() {
-        card = categoryService.demoteCardToFirst(card);
-    }
+  @Then("the card should be demoted to category {string}")
+  public void the_card_should_be_demoted_to_category(String expectedCategory) {
+    assertEquals(Category.valueOf(expectedCategory), card.getCategory());
+  }
 
-    @Then("the card should be demoted to category {string}")
-    public void the_card_should_be_demoted_to_category(String expectedCategory) {
-        assertEquals(Category.valueOf(expectedCategory), card.getCategory());
-    }
-    
-    @Then("the card should be marked as done")
-    public void the_card_should_be_marked_as_done() {
-        assertEquals(Category.DONE, card.getCategory());
-    }
-
+  @Then("the card should be marked as done")
+  public void the_card_should_be_marked_as_done() {
+    assertEquals(Category.DONE, card.getCategory());
+  }
 }
