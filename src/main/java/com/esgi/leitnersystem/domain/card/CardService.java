259a670e3ebf714b4557a001dbad37ce07dafebf
Restyled.io <commits@restyled.io>
2024-03-01 19:04:58 +0000

    Restyled by clang-format

--- src/main/java/com/esgi/leitnersystem/domain/card/CardService.java
+++ src/main/java/com/esgi/leitnersystem/domain/card/CardService.java
@@ -33,16 +33,17 @@ public class CardService {
   }
 
   public List<Card> fetchAllCards(Optional<List<String>> tags) {
-    return tags.map(cardRepository::findByTagsIn).orElseGet(cardRepository::findAll);
+    return tags.map(cardRepository::findByTagsIn)
+        .orElseGet(cardRepository::findAll);
   }
 
   public Card createCard(CardUserData cardUserData) {
     var card = Card.builder()
-            .question(cardUserData.getQuestion())
-            .answer(cardUserData.getAnswer())
-            .tag(cardUserData.getTag())
-            .category(Category.FIRST)
-            .build();
+                   .question(cardUserData.getQuestion())
+                   .answer(cardUserData.getAnswer())
+                   .tag(cardUserData.getTag())
+                   .category(Category.FIRST)
+                   .build();
 
     return cardRepository.save(card);
   }
@@ -50,8 +51,8 @@ public class CardService {
   public List<Card> getCardsForQuizz(LocalDate date) {
     var allCards = cardRepository.findAll();
     return allCards.stream()
-            .filter(card -> isEligibleForQuiz(card, date))
-            .collect(Collectors.toList());
+        .filter(card -> isEligibleForQuiz(card, date))
+        .collect(Collectors.toList());
   }
 
   private boolean isEligibleForQuiz(Card card, LocalDate date) {
@@ -59,22 +60,31 @@ public class CardService {
       return false;
     }
 
-    var lastRevision = cardRevisionRepository.findTopByCardIdOrderByRevisionDateDesc(card.getId());
+    var lastRevision =
+        cardRevisionRepository.findTopByCardIdOrderByRevisionDateDesc(
+            card.getId());
     return lastRevision.map(revision -> shouldBeReviewed(card, revision, date))
-            .orElse(true);
+        .orElse(true);
   }
 
-  private boolean shouldBeReviewed(Card card, CardRevision lastRevision, LocalDate currentDate) {
-    var lastRevisionDate = LocalDate.parse(lastRevision.getRevisionDate(), DateTimeFormatter.ISO_LOCAL_DATE);
-    long daysSinceLastRevision = ChronoUnit.DAYS.between(lastRevisionDate, currentDate);
-    int expectedDaysUntilNextRevision = categoryService.getDaysUntilNextRevision(card.getCategory());
+  private boolean shouldBeReviewed(Card card, CardRevision lastRevision,
+                                   LocalDate currentDate) {
+    var lastRevisionDate = LocalDate.parse(lastRevision.getRevisionDate(),
+                                           DateTimeFormatter.ISO_LOCAL_DATE);
+    long daysSinceLastRevision =
+        ChronoUnit.DAYS.between(lastRevisionDate, currentDate);
+    int expectedDaysUntilNextRevision =
+        categoryService.getDaysUntilNextRevision(card.getCategory());
     return daysSinceLastRevision >= expectedDaysUntilNextRevision;
   }
 
   @Transactional
-  public void processCardAnswer(UUID cardId, boolean isValid) throws CardNotFoundException {
-    var card = cardRepository.findById(cardId)
-            .orElseThrow(() -> new CardNotFoundException("Card with ID " + cardId + " not found"));
+  public void processCardAnswer(UUID cardId, boolean isValid)
+      throws CardNotFoundException {
+    var card = cardRepository.findById(cardId).orElseThrow(
+        ()
+            -> new CardNotFoundException("Card with ID " + cardId +
+                                         " not found"));
 
     if (isValid) {
       categoryService.promoteCard(card);
@@ -94,7 +104,8 @@ public class CardService {
   }
 
   private void recordCardRevision(Card card, boolean isValid) {
-    var formattedDate = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+    var formattedDate =
+        LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
     var revision = new CardRevision(card.getId(), formattedDate, isValid);
     cardRevisionRepository.save(revision);
   }
